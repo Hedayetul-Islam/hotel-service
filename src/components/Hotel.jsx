c8e379dd0b1b9f926f4f16b1676b2ead45dd72dd
Hedayetul-Islam <hedayetul36@gmail.com>
2023-10-05 22:17:41 +0600

    see more button working properly

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,23 +1,39 @@
-import { useEffect, useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import HotelCard from "./HotelCard";
 
 const Hotel = () => {
     const [hotels, setHotels] = useState([]);
+    const [visibleHotels, setVisibleHotels] = useState(6);
+    const [loadMore, setLoadMore] = useState(3);
 
     useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setHotels(data))
-    }, [])
+        fetch("data.json")
+            .then((res) => res.json())
+            .then((data) => setHotels(data));
+    }, []);
+
+    const showMoreHotels = () => {
+        setVisibleHotels((prevVisible) => prevVisible + loadMore);
+    };
 
     return (
-        <div className="grid grid-cols-3 gap-10">
-            {
-                hotels.map(hotel => <HotelCard
-                    key={hotel.id}
-                    hotel={hotel}
-                ></HotelCard>)
-            }
+        <div>
+            <div className="grid grid-cols-3 gap-10 mb-6">
+                {hotels.slice(0, visibleHotels).map((hotel) => (
+                    <HotelCard key={hotel.id} hotel={hotel}></HotelCard>
+                ))}
+            </div>
+            {visibleHotels < hotels.length && (
+                <h3 className="text-center">
+                    <button
+                        className="btn btn-primary mb-6"
+                        onClick={showMoreHotels}
+                    >
+                        Show More
+                    </button>
+                </h3>
+            )}
         </div>
     );
 };
